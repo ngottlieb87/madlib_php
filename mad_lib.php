d89391a3ea1e3c7a47e0602e6e5ea7c0d70f9fe3
ngottlieb87 <nogpdx@gmail.com>
2018-01-08 13:06:35 -0800

    simple mad lib using php

---
+++ mad_lib.php
@@ -0,0 +1,24 @@
+<?php
+    $person1 = $_GET["person1"];
+    $person2 = $_GET["person2"];
+    $animal = $_GET["animal"];
+    $exclamation = $_GET["exclamation"];
+    $verb = $_GET["verb"];
+    $noun = $_GET["noun"];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mad Lib</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+</head>
+<body
+  <div class ="container">
+    <h1>A fantastical adventure</h1>
+    <p>One day, <?php echo $person1 ?> and <?php echo $person2 ?> were walking through the woods, when suddenly a giant <?php echo $animal ?> appeared. </p>
+    <p>"<?php echo $exclamation ?>", <?php echo $person1 ?> cried. </p>
+    <p>The two of them <?php echo $verb ?> as quickly possible, and when they were safe, <?php echo $person1 ?> and <?php echo $person2 ?> gave each other a giant <?php echo $noun ?>.</p>
+  </div>
+</body>
+</html>
